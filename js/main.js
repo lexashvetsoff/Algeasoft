6a644dafdc863620aa1cb669318a7bb1f008c4d5
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2021-06-15 20:07:26 +0800

    создано модальное окно - благодарность

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     headerBtn.on("click", openModal);
     modalClose.on("click", closeModal);
 
+    var modalBtn = $('#form-btn');
+    var modalThanksClose = $('.modal-thanks__close');
+
+    modalBtn.on("click", clickFormBtn);
+    modalThanksClose.on("click", thanksClose);
+
     function openModal() {
         var modal = $('.modal-call');
         modal.addClass('modal-call--active');
@@ -14,5 +20,20 @@ $(document).ready(function() {
         event.preventDefault();
         var modal = $('.modal-call');
         modal.removeClass('modal-call--active');
+    }
+
+    function clickFormBtn(event) {
+        event.preventDefault();
+        var modal = $('.modal-call');
+        var thanks = $('.modal-thanks');
+
+        modal.removeClass('modal-call--active');
+        thanks.addClass('modal-thanks--active')
+    }
+
+    function thanksClose(event) {
+        event.preventDefault();
+        var thanks = $('.modal-thanks');
+        thanks.removeClass('modal-thanks--active');
     }
 });
